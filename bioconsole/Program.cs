f826498e512c0235db32cbf891086c4fb3b6f0f1
keegan.neave <keegan.neave@sa.catapult.org.uk>
2015-09-12 15:07:13 +0100

    Copied from the SDK, a list of all joints in the body and which connect.  I'll parse this to calculate distance between positions.

--- bioconsole/Program.cs
+++ bioconsole/Program.cs
@@ -12,9 +12,13 @@ namespace bioconsole
     {
         private static KinectSensor sensor = null;
         private static Body[] bodies = null;
+        private static List<Tuple<JointType, JointType>> bones;
 
         static void Main(string[] args)
         {
+            //  Create the list of joints
+            GenerateBones();
+
             sensor = KinectSensor.GetDefault();
             sensor.Open();
 
@@ -23,6 +27,46 @@ namespace bioconsole
 
             Console.WriteLine("Running.");
             Console.ReadLine();
+        }
+
+        private static void GenerateBones()
+        {
+            // a bone defined as a line between two joints
+            bones = new List<Tuple<JointType, JointType>>();
+
+            // Torso
+            bones.Add(new Tuple<JointType, JointType>(JointType.Head, JointType.Neck));
+            bones.Add(new Tuple<JointType, JointType>(JointType.Neck, JointType.SpineShoulder));
+            bones.Add(new Tuple<JointType, JointType>(JointType.SpineShoulder, JointType.SpineMid));
+            bones.Add(new Tuple<JointType, JointType>(JointType.SpineMid, JointType.SpineBase));
+            bones.Add(new Tuple<JointType, JointType>(JointType.SpineShoulder, JointType.ShoulderRight));
+            bones.Add(new Tuple<JointType, JointType>(JointType.SpineShoulder, JointType.ShoulderLeft));
+            bones.Add(new Tuple<JointType, JointType>(JointType.SpineBase, JointType.HipRight));
+            bones.Add(new Tuple<JointType, JointType>(JointType.SpineBase, JointType.HipLeft));
+
+            // Right Arm
+            bones.Add(new Tuple<JointType, JointType>(JointType.ShoulderRight, JointType.ElbowRight));
+            bones.Add(new Tuple<JointType, JointType>(JointType.ElbowRight, JointType.WristRight));
+            bones.Add(new Tuple<JointType, JointType>(JointType.WristRight, JointType.HandRight));
+            bones.Add(new Tuple<JointType, JointType>(JointType.HandRight, JointType.HandTipRight));
+            bones.Add(new Tuple<JointType, JointType>(JointType.WristRight, JointType.ThumbRight));
+
+            // Left Arm
+            bones.Add(new Tuple<JointType, JointType>(JointType.ShoulderLeft, JointType.ElbowLeft));
+            bones.Add(new Tuple<JointType, JointType>(JointType.ElbowLeft, JointType.WristLeft));
+            bones.Add(new Tuple<JointType, JointType>(JointType.WristLeft, JointType.HandLeft));
+            bones.Add(new Tuple<JointType, JointType>(JointType.HandLeft, JointType.HandTipLeft));
+            bones.Add(new Tuple<JointType, JointType>(JointType.WristLeft, JointType.ThumbLeft));
+
+            // Right Leg
+            bones.Add(new Tuple<JointType, JointType>(JointType.HipRight, JointType.KneeRight));
+            bones.Add(new Tuple<JointType, JointType>(JointType.KneeRight, JointType.AnkleRight));
+            bones.Add(new Tuple<JointType, JointType>(JointType.AnkleRight, JointType.FootRight));
+
+            // Left Leg
+            bones.Add(new Tuple<JointType, JointType>(JointType.HipLeft, JointType.KneeLeft));
+            bones.Add(new Tuple<JointType, JointType>(JointType.KneeLeft, JointType.AnkleLeft));
+            bones.Add(new Tuple<JointType, JointType>(JointType.AnkleLeft, JointType.FootLeft));
         }
 
         static void bfr_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
@@ -49,11 +93,12 @@ namespace bioconsole
             if (dataReceived)
             {
                 foreach (Body body in bodies)
-                {                    
+                {
                     if (body.IsTracked)
                     {
                         IReadOnlyDictionary<JointType, Joint> joints = body.Joints;
                         Console.WriteLine("Body detected at {0}", DateTime.Now);
+
                     }
                 }
             }

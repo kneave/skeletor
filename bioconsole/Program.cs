4390ed9af159f1903bab730441e239194a69299e
keegan.neave <keegan.neave@sa.catapult.org.uk>
2015-09-12 14:48:06 +0100

    Simple console app, body tracking working.

---
+++ bioconsole/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace bioconsole
+{
+    class Program
+    {
+        private static KinectSensor sensor = null;
+        private static Body[] bodies = null;
+
+        static void Main(string[] args)
+        {
+            sensor = KinectSensor.GetDefault();
+            sensor.Open();
+
+            BodyFrameReader bfr = sensor.BodyFrameSource.OpenReader();
+            bfr.FrameArrived += bfr_FrameArrived;
+
+            Console.WriteLine("Running.");
+            Console.ReadLine();
+        }
+
+        static void bfr_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            bool dataReceived = false;
+
+            using (BodyFrame bodyFrame = e.FrameReference.AcquireFrame())
+            {
+                if (bodyFrame != null)
+                {
+                    if (bodies == null)
+                    {
+                        bodies = new Body[bodyFrame.BodyCount];
+                    }
+
+                    // The first time GetAndRefreshBodyData is called, Kinect will allocate each Body in the array.
+                    // As long as those body objects are not disposed and not set to null in the array,
+                    // those body objects will be re-used.
+                    bodyFrame.GetAndRefreshBodyData(bodies);
+                    dataReceived = true;
+                }
+            }
+
+            if (dataReceived)
+            {
+                foreach (Body body in bodies)
+                {                    
+                    if (body.IsTracked)
+                    {
+                        IReadOnlyDictionary<JointType, Joint> joints = body.Joints;
+                        Console.WriteLine("Body detected at {0}", DateTime.Now);
+                    }
+                }
+            }
+        }
+    }
+}
